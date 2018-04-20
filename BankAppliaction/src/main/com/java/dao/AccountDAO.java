4b5e34fe8d79067bf863e1e4eca8fce93540eb01
PrzemyslawZnojek <przemek.znojek@gmail.com>
2018-04-20 15:55:59 +0200

    Adding Account dao Interface and dao Implementation class

---
+++ BankAppliaction/src/main/com/java/dao/AccountDAO.java
@@ -0,0 +1,5 @@
+package main.com.java.dao;
+
+public interface AccountDAO {
+
+}
--- BankAppliaction/src/main/com/java/entity/Account.java
+++ BankAppliaction/src/main/com/java/entity/Account.java
@@ -2,4 +2,14 @@ package main.com.java.entity;
 
 public class Account {
 
+	private String balance;
+
+	public String getBalance() {
+		return balance;
+	}
+
+	public void setBalance(String balance) {
+		this.balance = balance;
+	}
+	
 }
