571afafeb74e891a579d59238f5cda5fa959cf44
JakubGlegolaDev <jglegola6@gmail.com>
2018-04-20 16:58:38 +0200

    elo

--- BankAppliaction/src/main/com/java/entity/Account.java
+++ BankAppliaction/src/main/com/java/entity/Account.java
@@ -2,14 +2,42 @@ package main.com.java.entity;
 
 public class Account {
 
-	private String balance;
+	private long accountID;
+	private long customerID;
+	private long balance;
+	private String accountNumber;
+	
 
-	public String getBalance() {
+	public long getAccountID() {
+		return accountID;
+	}
+
+	public long getCustomerID() {
+		return customerID;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountID(long accountID) {
+		this.accountID = accountID;
+	}
+
+	public long getBalance() {
 		return balance;
 	}
 
-	public void setBalance(String balance) {
+	public void setBalance(long balance) {
 		this.balance = balance;
+	}
+	
+	public void setCustomerID(long customerID) {
+		this.customerID = customerID;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
 	}
 	
 }
--- BankAppliaction/src/main/com/java/entity/Customer.java
+++ BankAppliaction/src/main/com/java/entity/Customer.java
@@ -1,5 +1,36 @@
 package main.com.java.entity;
 
 public class Customer {
+	
+	private long customerID;
+	private String firstName;
+	private String lastName;
+	private String country;
+	public long getCustomerID() {
+		return customerID;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCustomerID(long customerID) {
+		this.customerID = customerID;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
 
+	
+	
 }
