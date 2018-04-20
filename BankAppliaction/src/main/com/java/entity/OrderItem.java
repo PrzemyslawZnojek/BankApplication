f8c835e2422be8593c42f39a999ce1c0a6ebdadf
PrzemyslawZnojek <przemek.znojek@gmail.com>
2018-04-20 16:51:38 +0200

    Getters and Setters generation.

--- BankAppliaction/src/main/com/java/entity/OrderItem.java
+++ BankAppliaction/src/main/com/java/entity/OrderItem.java
@@ -1,5 +1,43 @@
 package main.com.java.entity;
 
 public class OrderItem {
+	
+	private long orderItemID;
+	private long amount;
+	private String accountNumberSender = "";
+	private String accountNumberReceiver = "";
+	
+	public OrderItem(long orderItemID, long amount, String accountNumberSender, String accountNumberReceiver) {
+		this.orderItemID = orderItemID;
+		this.amount = amount;
+		this.accountNumberSender = accountNumberSender;
+		this.accountNumberReceiver = accountNumberReceiver;
+	}
+	
+	public long getOrderItemID() {
+		return orderItemID;
+	}
+	public void setOrderItemID(long orderItemID) {
+		this.orderItemID = orderItemID;
+	}
+	public long getAmount() {
+		return amount;
+	}
+	public void setAmount(long amount) {
+		this.amount = amount;
+	}
+	public String getAccountNumberSender() {
+		return accountNumberSender;
+	}
+	public void setAccountNumberSender(String accountNumberSender) {
+		this.accountNumberSender = accountNumberSender;
+	}
+	public String getAccountNumberReceiver() {
+		return accountNumberReceiver;
+	}
+	public void setAccountNumberReceiver(String accountNumberReceiver) {
+		this.accountNumberReceiver = accountNumberReceiver;
+	}
 
+	
 }
