abb7794332cbd90284948b068dd712ff5c826e6d
PrzemyslawZnojek <przemek.znojek@gmail.com>
2018-05-06 16:44:33 +0200

    adding security

--- BankAppliaction/src/main/com/java/controller/HomeController.java
+++ BankAppliaction/src/main/com/java/controller/HomeController.java
@@ -1,14 +1,14 @@
-package main.com.java.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class HomeController {
-
-	@RequestMapping("/")
-	public String loggingView() {
-		return "logging-view";
-		
-	}
-}
+//package main.com.java.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class HomeController {
+//
+//	@RequestMapping("/")
+//	public String loggingView() {
+//		return "logging-view";
+//		
+//	}
+//}
