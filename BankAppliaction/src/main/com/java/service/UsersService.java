f2c0a2ba5f681dd6463d1a98ab29cbfa3abbc526
JakubGlegolaDev <jglegola6@gmail.com>
2018-05-09 21:34:47 +0200

    service

---
+++ BankAppliaction/src/main/com/java/service/UsersService.java
@@ -0,0 +1,9 @@
+package main.com.java.service;
+
+import main.com.java.entity.Account;
+
+public interface UsersService {
+
+	public void addAccount(Account theAccount);
+
+}
---
+++ BankAppliaction/src/main/com/java/service/UsersServiceImpl.java
@@ -0,0 +1,13 @@
+package main.com.java.service;
+
+import main.com.java.entity.Account;
+
+public class UsersServiceImpl implements UsersService {
+
+	@Override
+	public void addAccount(Account theAccount) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
