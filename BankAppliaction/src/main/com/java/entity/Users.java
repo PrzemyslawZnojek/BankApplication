22589e0894dac95ee26813184ea99a668bf415b8
PrzemyslawZnojek <przemek.znojek@gmail.com>
2018-05-09 21:33:46 +0200

    dao and daoImpl

--- BankAppliaction/src/main/com/java/entity/Users.java
+++ BankAppliaction/src/main/com/java/entity/Users.java
@@ -29,5 +29,48 @@ public class Users {
 	
 	@OneToOne(mappedBy="users", cascade=CascadeType.ALL)
 	private Authorities authorities;
+
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+	public Authorities getAuthorities() {
+		return authorities;
+	}
+
+	public void setAuthorities(Authorities authorities) {
+		this.authorities = authorities;
+	}
+	
+	
 	
 }
