d7ef5dda157eb64aa507eceda1785fc80d5d7e51
PrzemyslawZnojek <przemek.znojek@gmail.com>
2018-07-10 00:13:31 +0200

    Deleting Jars from build path and add that in pom.xml

--- insert.sql
+++ insert.sql
@@ -3,13 +3,13 @@ USE `bank_application`;
 
 INSERT INTO `users` 
 VALUES 
-('john','{noop}test123',1),
-('susan','{noop}test123',1);
+('karol','{noop}test123',1);
 
 
 INSERT INTO `authorities` 
 VALUES 
-('john','ROLE_USER'),
-('susan','ROLE_EMPLOYEE');
+('karol','ROLE_USER');
+
+
 
 
