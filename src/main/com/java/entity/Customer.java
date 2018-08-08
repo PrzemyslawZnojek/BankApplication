d70179c7abb934a8c6a6e38c07864daf24e46ce2
PrzemyslawZnojek <przemek.znojek@gmail.com>
2018-08-08 23:04:38 +0200

    Changing mapping in Users, Account and Customer

--- src/main/com/java/entity/Customer.java
+++ src/main/com/java/entity/Customer.java
@@ -1,14 +1,6 @@
 package main.com.java.entity;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-
+import javax.persistence.*;
 
 
 @Entity
@@ -19,7 +11,10 @@ public class Customer {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="customerID")
 	private long customerID;
-	
+
+	@Column(name="idOfAccount")
+	private long idOfAccount;
+
 	@Column(name="firstName")
 	private String firstName;
 	
@@ -28,58 +23,44 @@ public class Customer {
 	
 	@Column(name="country")
 	private String country;
-	
-	@OneToOne(mappedBy="customer", cascade=CascadeType.ALL)
+
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="idOfAccount", insertable = false, updatable = false)
 	private Account account;
 
 	public Customer() {}
 	
-	public Customer(String firstName, String lastName, String country) {	
-		this.firstName = firstName;
+	public Customer(long idOfAccount, String firstName, String lastName, String country) {
+		this.idOfAccount = idOfAccount;
+	    this.firstName = firstName;
 		this.lastName = lastName;
 		this.country = country;
 	}
-	
-	public long getCustomerID() {
-		return customerID;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public String getCountry() {
-		return country;
-	}
-	public void setCustomerID(long customerID) {
-		this.customerID = customerID;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public void setCountry(String country) {
-		this.country = country;
-	}
-	
-	public Account getAccount() {
-		return account;
-	}
 
-	public void setAccount(Account account) {
-		this.account = account;
-	}
+    public long getCustomerID() {
+        return customerID;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public Account getAccount() {
+        return account;
+    }
 
-	
-	// for log diagnose.
-	@Override
-	public String toString() {
-		return "Customer [customerID=" + customerID + ", firstName=" + firstName + ", lastName=" + lastName
-				+ ", country=" + country + "]";
-	}
+    public void setIdOfAccount(long idOfAccount) { this.idOfAccount = idOfAccount;}
+    public void setAccount(Account account) { this.account = account; }
+
+    @Override
+    public String toString() {
+        return "Customer [customerID=" + customerID + ", firstName=" + firstName + ", lastName=" + lastName
+                + ", country=" + country + "]";
+    }
 
 	
 	
--- src/main/com/java/entity/Users.java
+++ src/main/com/java/entity/Users.java
@@ -1,36 +1,31 @@
 package main.com.java.entity;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="users")
 public class Users {
-	
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="username")
 	private String username;
-	 
+
 	@Column(name="password")
 	private String password;
-	
+
 	@Column(name="enabled")
 	private boolean enabled;
 
-	@OneToOne(mappedBy="users", cascade=CascadeType.ALL)
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="username", insertable = false, updatable = false)
 	private Account account;
-	
+
 	@OneToOne(mappedBy="users", cascade=CascadeType.ALL)
 	private Authorities authorities;
 
-	
+
+
 	public String getUsername() {
 		return username;
 	}
