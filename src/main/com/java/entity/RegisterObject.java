4d7d601f5b345eba60f51401c77d70ececb0659b
PrzemyslawZnojek <przemek.znojek@gmail.com>
2018-08-08 22:56:42 +0200

    Adding RegisterObject

---
+++ src/main/com/java/entity/RegisterObject.java
@@ -0,0 +1,32 @@
+package main.com.java.entity;
+
+public class RegisterObject {
+
+    private Account account;
+    private Customer customer;
+    private Users users;
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Users getUsers() {
+        return users;
+    }
+
+    public void setUsers(Users users) {
+        this.users = users;
+    }
+}
