c6739e531860baa18b3a1d5f3be62e1da65425be
Jake <jglegola6@gmail.com>
2018-11-14 20:23:48 +0100

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -28,8 +28,6 @@ VALUES
 INSERT INTO `bank_application`.`authorities` 
 VALUES 
 ('john','ROLE_EMPLOYEE'),
-('mary','ROLE_EMPLOYEE'),
-('mary','ROLE_MANAGER'),
-('susan','ROLE_EMPLOYEE'),
-('susan','ROLE_ADMIN');
+('mary','ROLE_USER'),
+('susan','ROLE_EMPLOYEE');
 
